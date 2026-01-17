92e29df836362a5a9b3f2ca8d607a37bb6c654a8
Evgeny lesnikova <zhenya.karases@list.ru>
2026-01-17 18:37:48 +0500

    Add files via upload

---
+++ lab 3/1 task.py
@@ -0,0 +1,14 @@
+def find_item_index(items, target):
+    for i in range(len(items)):
+        if items[i] == target:
+            return i
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_index(items_list, find_item)  # Вызов функции
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
